5b776b39c10ded36a2c0ceb6c78ee6c5b7e1dec3
arcaner666 <arcaner666@gmail.com>
2021-03-06 00:15:35 +0300

    updated

--- src/app/components/public-navigation/public-navigation.component.ts
+++ src/app/components/public-navigation/public-navigation.component.ts
@@ -16,6 +16,6 @@ export class PublicNavigationComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
 }
