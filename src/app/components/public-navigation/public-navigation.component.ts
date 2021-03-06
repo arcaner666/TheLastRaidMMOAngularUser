464092e825071bb694a7fada0e2f9cda90707324
arcaner666 <arcaner666@gmail.com>
2021-03-06 17:34:37 +0300

    updated

--- src/app/components/public-navigation/public-navigation.component.ts
+++ src/app/components/public-navigation/public-navigation.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-public-navigation',
   templateUrl: './public-navigation.component.html',
   styleUrls: ['./public-navigation.component.css']
 })
+
 export class PublicNavigationComponent implements OnInit {
 
+  public innerWidth: any;
   isSidenavActive: boolean = false;
+  onMobile: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.innerWidth = window.innerWidth;
+  }
+
+  OnMobile() {
+    if (this.innerWidth <= 730) {
+      this.onMobile = true;
+    }
+    else {
+      this.onMobile = false;
+    }
+  }
+
+  // Uygulamanın açık olduğu sekmenin boyutu her değiştiğinde bize ekran genişliğini pixel olarak döndürür.
+  @HostListener('window:resize', ['$event'])
+  OnResize(event: any) {
+    this.innerWidth = window.innerWidth;
+    this.OnMobile();
   }
 
 }
