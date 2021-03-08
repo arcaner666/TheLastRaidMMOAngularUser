bd200f9c32e7fbd40fb37b285fae089540dcd317
arcaner666 <arcaner666@gmail.com>
2021-03-08 18:14:06 +0300

    updated

--- src/app/components/public-register/public-register.component.ts
+++ src/app/components/public-register/public-register.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './../../services/auth.service';
+import { Player } from 'src/app/models/Player';
+import { Result } from 'src/app/models/Result';
 
 @Component({
   selector: 'app-public-register',
   templateUrl: './public-register.component.html',
   styleUrls: ['./public-register.component.css']
 })
-export class PublicRegisterComponent implements OnInit {
+export class PublicRegisterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  player: Player = new Player();
+
+  checkNameResult: Result = new Result();
+  registerResult: Result = new Result();
+
+  passwordAgain: string = "";
+  emailAgain: string = "";
+
+  sub1: any;
+  sub2: any;
+
+  constructor(
+    public auth: AuthService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(): void {
+    if (this.sub1) {
+      this.sub1.unsubscribe();
+    }
+    if (this.sub2) {
+      this.sub2.unsubscribe();
+    }
+  }
+
+  Register(player: Player) {
+    this.registerResult = new Result();
+    this.checkNameResult = new Result();
+    this.sub1 = this.auth.CheckUserNameAvailability(player.UserName).subscribe(a => {
+      if (a) {
+        this.checkNameResult.isDone = false;
+        this.checkNameResult.info = "This user name is already in use.";
+      }
+      else {
+        var date: Date = new Date();
+        this.player.ConfirmationCode = this.auth.GenerateToken(6);
+        this.player.ConfirmationDate = date.getTime().toString();
+        this.sub2 = this.auth.Register(player).subscribe((b: any) => {
+          console.log(b);
+          this.player = new Player();
+          this.passwordAgain = "";
+          this.emailAgain = "";
+          this.registerResult.isDone = true;
+          this.registerResult.info = "Registration successful.";
+          //document.getElementById("openRegistrationModal").click();
+        }, error => {
+          this.registerResult.isDone = false;
+          this.registerResult.info = "Registration failed!" + error;
+          //document.getElementById("openRegistrationModal").click();
+        });
+      }
+    });
+  }
+
+  Reset() {
+    this.checkNameResult = new Result();
+  }
 }
