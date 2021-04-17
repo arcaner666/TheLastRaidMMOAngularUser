14ba0060de4ff983b91af27af1c5551a63b06aca
Arcan Caner Şenol <arcaner666@gmail.com>
2021-04-17 17:45:48 +0300

    updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TheLastRaidMMO
+# TheLastRaidMMOAngularUser
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.3.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/TheLastRaidMMO'),
+      dir: require('path').join(__dirname, './coverage/TheLastRaidMMOAngularUser'),
       subdir: '.',
       reporters: [
         { type: 'html' },
